460217972319dbc62cdb8a29ae53fc8faa81602f
Julio Luque <julio.a.luque@gmail.com>
2020-04-30 19:31:55 -0300

    Prototype y pruebas de hashcode

--- src/ar/jluque/ioc/MainSingletonPrototype.java
+++ src/ar/jluque/ioc/MainSingletonPrototype.java
@@ -11,14 +11,42 @@ public class MainSingletonPrototype {
 //		peticion de beans
 		SecretarioEmpleado delfi = context.getBean("miSecretario", SecretarioEmpleado.class);
 		SecretarioEmpleado ceci = context.getBean("miSecretario", SecretarioEmpleado.class);
+		SecretarioEmpleado a = context.getBean("miSecretario", SecretarioEmpleado.class);
+		SecretarioEmpleado b = context.getBean("miSecretario", SecretarioEmpleado.class);
+		SecretarioEmpleado c = context.getBean("miSecretario", SecretarioEmpleado.class);
+		SecretarioEmpleado d = context.getBean("miSecretario", SecretarioEmpleado.class);
+		SecretarioEmpleado e = context.getBean("miSecretario", SecretarioEmpleado.class);
+		SecretarioEmpleado f = context.getBean("miSecretario", SecretarioEmpleado.class);
+		SecretarioEmpleado g = context.getBean("miSecretario", SecretarioEmpleado.class);
 
-		System.out.println(delfi);	//ar.jluque.ioc.SecretarioEmpleado@26aa12dd  <- ambos apuntan a la misma direccion en memoria
-		System.out.println(ceci);	//ar.jluque.ioc.SecretarioEmpleado@26aa12dd  <- ambos apuntan a la misma direccion en memoria
+		System.out.println(delfi);	
+		System.out.println(ceci);	
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
 
-		if(delfi.equals(ceci)) {
+		
+//		CON SINGLETON
+//		ar.jluque.ioc.SecretarioEmpleado@26aa12dd  <- ambos apuntan a la misma direccion en memoria
+//		ar.jluque.ioc.SecretarioEmpleado@26aa12dd  <- ambos apuntan a la misma direccion en memoria
+		
+//		CON PROTOTYPE
+//		ar.jluque.ioc.SecretarioEmpleado@26aa12dd
+//		ar.jluque.ioc.SecretarioEmpleado@3fd7a715
+		
+		/**
+		 * La configuracion en applicationContext scope="singleton" / scope"prototype" hace que el objeto se ubique en posiciones diferentes de memmoria. por ende,  
+		 */
+		 
+		if(delfi.equals(ceci)) {  	//scope="singleton"
 			System.out.println("mismo hashcode: " + ceci.hashCode());
 			System.out.println("mismo hashcode: " + delfi.hashCode());
-		} else {
+			
+		} else {					//scope"prototype"
 			System.out.println("distinto hashcode: " + ceci.hashCode());
 			System.out.println("distinto hashcode: " + delfi.hashCode());
 		}
